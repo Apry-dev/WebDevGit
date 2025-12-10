3a21a16530f2e839cafa239b734fd9b34bfc4b69
Apry-dev <C22488994@mytudublin.ie>
2025-12-10 14:05:11 +0000

    test commit

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-const router = require("express").Router();
-const auth = require("../middleware/auth");
-const orderController = require("../controllers/orderController");
-
-// ALL ROUTES ARE PROTECTED - require authentication
-router.post("/", auth, orderController.createOrder);
-router.get("/my", auth, orderController.getMyOrders);
-
-module.exports = router;
-
-=======
 const router = require('express').Router();
 const auth = require('../middleware/auth');
 const orderCtrl = require('../controllers/orderController');
@@ -18,4 +6,3 @@ router.post('/', auth, orderCtrl.createOrder);
 router.get('/me', auth, orderCtrl.listMyOrders);
 
 module.exports = router;
->>>>>>> de9d45130569532228cea5ce9f0a9a01a7ac49ce
